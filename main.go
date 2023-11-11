b8d25597fbefe2059c16de3252ed9222fee363d8
DiazAdriel0 <diazadriel0@gmail.com>
2023-11-11 15:09:22 -0300

    add(variables): parameters and return multiple values

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/diazadriel0/go-course/variables"
 )
 
 func main() {
 	variables.ShowInt()
 	variables.RestVariables()
+
+	status, text := variables.ConvertToText(int(variables.Salary))
+	fmt.Println(status)
+	fmt.Println(text)
 }
